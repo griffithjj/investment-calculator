deb6e93d0776275106fd965c15fbc6728eac6519
Jürgen Loff <jj.loff@gmail.com>
2025-02-04 23:07:18 +0200

    Edit UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QLabel>
+#include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
@@ -10,7 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
     m_ContributionIncreaseEdit(new QLineEdit),
     m_CalculateButton(new QPushButton("Calculate")),
     m_ResetButton(new QPushButton("Reset"))
-{}
+{
+    setup();
+}
 
 void MainWindow::setup(){
 
@@ -19,6 +22,27 @@ void MainWindow::setup(){
     QLabel* periodLabel{new QLabel("Investement Period (months)")};
     QLabel* contributionLabel{new QLabel("Monthly Contribution")};
     QLabel* contributionIncreaseLabel{new QLabel("Contribution Increase")};
+
+    QWidget* centralWidget{new QWidget};
+    QGridLayout* layout{new QGridLayout(this)};
+
+    layout->addWidget(initialInvestmentLabel, 0,0);
+    layout->addWidget(m_InitialEdit,1,0);
+    layout->addWidget(interestLabel, 2,0);
+    layout->addWidget(m_InterestEdit, 3,0);
+    layout->addWidget(periodLabel,4,0);
+    layout->addWidget(m_PeriodEdit, 5, 0);
+    layout->addWidget(contributionLabel,6,0);
+    layout->addWidget(m_ContributionEdit, 7, 0);
+    layout->addWidget(contributionIncreaseLabel, 8, 0);
+    layout->addWidget(m_ContributionIncreaseEdit, 9, 0);
+    layout->addWidget(m_CalculateButton, 11, 0);
+    layout->addWidget(m_ResetButton, 12, 0);
+
+    centralWidget->setLayout(layout);
+
+    this->setCentralWidget(centralWidget);
+    this->adjustSize();
 }
 
 MainWindow::~MainWindow() {}
